ff67679021ad66190c9a5a9a567135ad1568070b
blessonjohny <blessonjohny@gmail.com>
2026-08-27 11:27:50 -0400

    Add files via upload

--- 4p2p/server.js
+++ 4p2p/server.js
@@ -1480,8 +1480,8 @@ function computeTableDisplayName(seats, creatorName, existingGenericNames) {
 // means a malicious client can never get an arbitrary value reflected into other players'
 // pages through this field.
 const VALID_AVATAR_KEYS = new Set([
-  ...Array.from({length:50}, (_,i) => 'hero3f'+(i+1)),
-  ...Array.from({length:50}, (_,i) => 'hero3m'+(i+1)),
+  ...Array.from({length:36}, (_,i) => 'hero3f'+(i+1)),
+  ...Array.from({length:64}, (_,i) => 'hero3m'+(i+1)),
 ]);
 function sanitizeAvatarKey(k) { return (typeof k === 'string' && VALID_AVATAR_KEYS.has(k)) ? k : null; }
 
